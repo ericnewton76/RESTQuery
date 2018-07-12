2124bc5b68284711e6d6ad17148da2b92dab8235
Eric Newton <ericnewton76@gmail.com>
2018-07-12 00:00:09 -0400

    added FilterOptions_Tests

---
+++ src/RESTQueryTests/FilterOptions_Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using FluentAssertions;
+using RESTQuery;
+
+namespace RESTQueryTests
+{
+	[TestFixture]
+	public class FilterOptions_Tests
+	{
+
+		[Test]
+		public void ctor_throws_with_null()
+		{
+			Assert.Throws<ArgumentNullException>(() => {
+				var f = new FilterOptions(null, Operator.Equal, "");
+			});
+		}
+
+	}
+}
