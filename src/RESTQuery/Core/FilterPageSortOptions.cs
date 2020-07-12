46d2681106a60bd8bc475a6c5af1ba3851ec651f
Eric Newton <ericnewton76@gmail.com>
2020-07-12 18:55:52 -0400

    add convience method for Adding filters

--- src/RESTQuery/Core/FilterPageSortOptions.cs
+++ src/RESTQuery/Core/FilterPageSortOptions.cs
@@ -39,32 +39,60 @@ namespace RESTQuery
 		{
 			if (filterOption != null)
 			{
+				_AddFilter(filterOption);
+			}
+
+			return this;
+		}
+		/// <summary>
+		/// Adds a filter to filter options
+		/// </summary>
+		/// <param name="field">Name of field to filter on</param>
+		/// <param name="operator">an operator to use</param>
+		/// <param name="value">value to filter against</param>
+		/// <returns></returns>
+		public FilterPageSortOptions AddFilter(string field, Operator @operator, string value)
+		{
+			var filterOption = new FilterOptions(field, @operator, value);
+
+			this._AddFilter(filterOption);
+
+			return this;
+		}
+
+		private void _AddFilter(FilterOptions filterOption)
+		{
+			if (filterOption != null)
+			{
 				List<FilterOptions> filters = this.Filters as List<FilterOptions>;
-				if (filters == null) { filters = this.Filters.ToList(); this.Filters = filters; }
+				if (filters == null) 
+				{
+					//convert to List
+					filters = this.Filters.ToList();
+					this.Filters = filters; 
+				}
 
 				filters.Add(filterOption);
 			}
-
-			return this;
 		}
 
-		/// <summary>
-		/// Adds a sorting criteria to the sort options
-		/// </summary>
-		/// <param name="sortOption"></param>
-		/// <returns></returns>
-		public FilterPageSortOptions AddSort(SortOption sortOption)
-		{
-			if (sortOption != null)
+			/// <summary>
+			/// Adds a sorting criteria to the sort options
+			/// </summary>
+			/// <param name="sortOption"></param>
+			/// <returns></returns>
+			public FilterPageSortOptions AddSort(SortOption sortOption)
 			{
-				List<SortOption> sorts = this.Sort as List<SortOption>;
-				if (sorts == null) { sorts = this.Sort.ToList(); this.Sort = sorts; }
+				if (sortOption != null)
+				{
+					List<SortOption> sorts = this.Sort as List<SortOption>;
+					if (sorts == null) { sorts = this.Sort.ToList(); this.Sort = sorts; }
 
-				sorts.Add(sortOption);
+					sorts.Add(sortOption);
+				}
+
+				return this;
 			}
-
-			return this;
-		}
 
 	}
 
